3aab62d8f12634c035c817b21c74ad36d688ce4a
浮生若梦 <microease@163.com>
2019-03-07 16:44:03 +0800

    add

---
+++ Week3/Aplusplus.java
@@ -0,0 +1,14 @@
+import Math;
+
+public class Aplusplus {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = a++;
+//        int c = ++a;
+//        int d = a++ + ++a;
+        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+
+    }
+}
